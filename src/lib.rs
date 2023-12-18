0d9023bddff49a0460b1a00716bf7aafed20eb3a
Andrea Pelloni <apelloni90@gmail.com>
2023-12-18 12:01:28 +0100

    clean

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,8 @@
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
-//
-//use num_traits::{Float, FloatConst, FromPrimitive, Num, Signed, ToPrimitive, Zero};
-//
+
 //use itertools::Itertools;
-//
-//use num::traits::{NumAssign, NumOps, NumRef};
-//use num::NumCast;
-//use num::{BigInt, BigRational};
-//use std::fmt::{Debug, Display, LowerExp};
-//use std::iter::Sum;
-//use std::ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign};
-////use utils::Signum;
-////pub mod test;
-//
 // TODO: use it
 pub const MAX_VARIABLE: usize = 10;
 
